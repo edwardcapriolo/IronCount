fd4acd6a0835eb793630a1d7031be4d392027c5b
Edward Capriolo <edlinuxguru@gmail.com>
2012-02-10 08:34:13 -0500

    This was missed as part of the last commit.

---
+++ src/test/java/com/jointhegrid/ironcount/ClassNotFoundTestIntegrationTest.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.jointhegrid.ironcount;
+
+import java.util.HashMap;
+import java.util.Properties;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author edward
+ */
+public class ClassNotFoundTestIntegrationTest
+        extends IronIntegrationTest {
+
+   @Test
+  public void disableWorkload(){
+    Workload w = new Workload();
+    w.active = true;
+    w.consumerGroup = "group1";
+    w.maxWorkers = 4;
+    w.messageHandlerName = "bad.class.name";
+    w.name = "testworkload";
+    w.properties = new HashMap<String, String>();
+    w.topic = topic;
+    w.zkConnect = "localhost:8888";
+
+    Properties p = new Properties();
+    p.put(WorkloadManager.ZK_SERVER_LIST, "localhost:8888");
+    WorkloadManager m = new WorkloadManager(p);
+    m.init();
+
+    //dl.startWorkload(w);
+    m.startWorkload(w);
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException ex) {
+    }
+    Assert.assertEquals(0, m.getWorkerThreads().size() );
+    m.shutdown();
+
+
+  }
+}

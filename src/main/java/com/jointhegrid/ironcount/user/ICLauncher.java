10b8325d42bcc9dc83da83081b8818250a457f17
Edward Capriolo <edlinuxguru@gmail.com>
2012-03-15 19:23:48 -0400

    Dump ass static modifier was preventing two instances from running in the same JVM/IDE. Got I hate static

--- src/main/java/com/jointhegrid/ironcount/user/ICLauncher.java
+++ src/main/java/com/jointhegrid/ironcount/user/ICLauncher.java
@@ -12,7 +12,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
+ */
 package com.jointhegrid.ironcount.user;
 
 import com.jointhegrid.ironcount.manager.WorkloadManager;
@@ -22,17 +22,20 @@ import org.apache.log4j.Logger;
 public class ICLauncher {
 
   final static Logger logger = Logger.getLogger(ICLauncher.class.getName());
-  private static WorkloadManager workloadManager;
-  private static Properties properties;
-  
-  public static void main(String[] args) {
+  private WorkloadManager workloadManager;
+  private Properties properties;
+
+  public ICLauncher() {
     properties = System.getProperties();
     if (properties.get(WorkloadManager.ZK_SERVER_LIST) == null) {
-      logger.warn(WorkloadManager.ZK_SERVER_LIST+" was not defined. Defaulting to localhost:2181");
+      logger.warn(WorkloadManager.ZK_SERVER_LIST + " was not defined. Defaulting to localhost:2181");
       properties.setProperty(WorkloadManager.ZK_SERVER_LIST, "localhost:2181");
     }
-
     workloadManager = new WorkloadManager(properties);
     workloadManager.init();
+  }
+
+  public static void main(String[] args) {
+    new ICLauncher();
   }
 }
